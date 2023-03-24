65bc52fd9adcf9d3208e87abbb295f3ba0e7d69d
syh-RYAN <dudgh9477@naver.com>
2023-03-24 11:47:24 +0900

    Basic

---
+++ Day01/src/mycom/test/Main.java
@@ -0,0 +1,16 @@
+package mycom.test;
+
+import java.nio.file.SecureDirectoryStream;
+
+public class Main {
+	public static void main(String[] args) {
+		//Data
+		int x = 30;
+		int y = 40;
+		int z = 0;
+		
+		//명령어들의 집합
+		z=x+y;
+		System.out.println(z);
+	}
+};
